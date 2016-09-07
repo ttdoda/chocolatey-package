37fa3c286c0ecbb84122dd6fc6b27dd2cf6dc235
IWAMOTO Kouichi <sue@iwmt.org>
2016-09-07 17:57:41 +0900

    Tera Term 4.92 用 Chocolatey Package (試作品)

---
+++ teraterm/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'teraterm'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://osdn.jp/dl/ttssh2/teraterm-4.92.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Tera Term *'
+
+  checksum      = '4B42863FEFA83DAE0140F531939D334061F210F4092FA02B2E02063ABDC94EC0'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
