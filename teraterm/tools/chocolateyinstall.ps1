49fe6f38cfd5a1a51d61599a746ecfad84429350
IWAMOTO Kouichi <sue@iwmt.org>
2017-06-19 16:33:39 +0900

    moderator の review を反映

--- teraterm/tools/chocolateyinstall.ps1
+++ teraterm/tools/chocolateyinstall.ps1
@@ -3,21 +3,17 @@
 $packageName= 'teraterm'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://osdn.net/dl/ttssh2/teraterm-4.95.exe'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'Tera Term *'
 
   checksum      = '1820fcafd862ba345127375226eb38fd9094b0ece1964f90d72245e58f7facfd'
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
