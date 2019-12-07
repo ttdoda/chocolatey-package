07af9220484099f2595e261987233a57d9b23035
IWAMOTO Kouichi <sue@iwmt.org>
2019-12-08 00:54:20 +0900

    Tera Term 4.105

--- teraterm/tools/chocolateyinstall.ps1
+++ teraterm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'teraterm'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://osdn.net/dl/ttssh2/teraterm-4.104.exe'
+$url        = 'https://osdn.net/dl/ttssh2/teraterm-4.105.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   softwareName  = 'Tera Term *'
 
-  checksum      = '377a26c8fd29dfd91c174cb973fe06c969cd8d312b364ce8515675eb6d5c819b'
+  checksum      = '6ea1b0a64533014ed4cb92f0d496b73f242bf10d2a9a5af62f3439f9dcb01245'
   checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
